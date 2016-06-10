5df9d83b1301a150567bbd66e3d120178ee5bc3b
JD McKinstry <jdmckinstry@gmail.com>
2016-06-10 17:42:13 -0400

    Update Date.ext.js

--- Date.ext.js
+++ Date.ext.js
@@ -1,4 +1,3 @@
-
 /**	Date[extensions]()
  *	Date.add[Years|Months|Weeks|Days|Hours|Minutes|Seconds]()
  **/
@@ -11,6 +10,32 @@
 			'addHours': function(v) { this.setHours(this.getHours() + parseFloat(v)); return this; },
 			'addMinutes': function(v) { this.setMinutes(this.getMinutes() + parseFloat(v)); return this; },
 			'addSeconds': function(v) { this.setSeconds(this.getSeconds() + parseFloat(v)); return this; },
+		};
+	for (var k in methods) {
+		var v = methods[k];
+		Object['defineProperty'] && !Date.prototype.hasOwnProperty(k)
+			? Object.defineProperty(Date.prototype, k, { value: v }) : Date.prototype[k] = v;
+	}
+})();
+;(function() {	//	Date.getDayString
+	var methods = {
+			'getDayString': function(v) {
+				var days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday" ];
+				return days[this.getDay()];
+			}
+		};
+	for (var k in methods) {
+		var v = methods[k];
+		Object['defineProperty'] && !Date.prototype.hasOwnProperty(k)
+			? Object.defineProperty(Date.prototype, k, { value: v }) : Date.prototype[k] = v;
+	}
+})();
+;(function() {	//	Date.getWeek
+	var methods = {
+			'getWeek': function(v) {
+				var a = new Date(this.getFullYear(), 0, 1);
+				return Math.ceil(((this - a) / 864E5 + a.getDay() + 1) / 7)
+			}
 		};
 	for (var k in methods) {
 		var v = methods[k];
